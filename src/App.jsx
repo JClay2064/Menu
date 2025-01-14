0af85da66ad279bafc441a9ad28b5dd253cc2c77
JClay2064 <j.clay.2064@gmail.com>
2025-01-14 02:06:32 -0600

    Project setupp

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Title } from "./Title";
+const App = () => {
+  return(
+  <main>
+    <setion className='menu' >
+      <Title text={'our menu'}/>
+    </setion>
+  </main>
+  ) 
+};
+export default App;
