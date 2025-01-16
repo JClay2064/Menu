133a4b02a3ab8b54b240f463691ea96dee1197e7
JClay2064 <j.clay.2064@gmail.com>
2025-01-16 03:15:41 -0600

    Added Categories State and props w/buttons for selection

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,23 @@ import { Title } from "./Title";
 import menu from "./data";
 import { useState } from "react";
 import { Menu } from "./Menu";
+import Categories from "./Categories";
 
 
+
+const allCategories = ['all', ...new Set(menu.map((item) => item.category))];
 
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+  console.log(categories);
   
   
   return(
   <main>
     <setion className='menu' >
       <Title text={'our menu'}/>
+      <Categories categories={categories} filterItems={setMenuItems}/>
       <Menu items={menuItems}/>
     </setion>
   </main>
